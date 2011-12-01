c74d7b5bbaf06610b9ea25df049b64bc383b06ab
Silvestre Huens <s.huens@gmail.com>
2011-12-01 15:47:57 -0300

    galeria para videos

--- telesur/theme/static/article.js
+++ telesur/theme/static/article.js
@@ -1,10 +1,37 @@
 jq(function($) {
-    $(".video-galery-container").scrollable({size: 1, circular: true,});
+
+    /*we have to pre preocess the html in order to get the correct gallery disposition*/
     
-    /*ESTE JS SE LLAMA 2 VECES, CUIDADO!!!!!, probablmenete por diazo*/
-    $(".video-counter a").prepOverlay({
+    /*lest do a relatinship thumb => video*/
+    $('#video-galery-container .scrollable-video-galery .thumb-image').each(function(){
+        $(this).data('video', $(this).parent('.video-widget'));
+    });
+    
+    /*moving thumbs into his own div*/
+    var thumbs = $('#video-galery-container .scrollable-video-galery .thumb-image').detach();
+    var items = $('.scrollable-video-galery').append('<div class="items"/>').find('.items');
+    items.html(thumbs);
+    
+    /*moving videos outside the container into the video wrapper*/
+    var videos = $('.scrollable-video-galery .video-widget').detach();
+    $('.videos-wrapper').html(videos);
+    
+    /*lets hide all the videos but the first*/
+    $('.videos-wrapper .video-widget').css('display', 'none').filter(":first").css('display', 'block');
+    
+    $(".video-counter .counter").prepOverlay({
         subtype:'inline',
         target: '#video-galery-container',
+    });
+
+    $("#video-galery-container").scrollable({size: 4});
+    
+    
+    /*functionality for the gallery*/
+    /*thumbs selectors*/
+    $('.items .thumb-image').click(function(){
+        $('.videos-wrapper .video-widget').hide();
+        $(this).data('video').show();
     });
             
 });
