655bac86468295462cfbdd8ba33ae7283b3544e9
hvelarde <hector.velarde@gmail.com>
2012-01-19 17:30:31 -0600

    se actualizan los clasificadores del paquete

--- setup.py
+++ setup.py
@@ -8,13 +8,17 @@ setup(name='telesur.theme',
       description="Un tema Diazo en Plone 4.1 para el sitio de teleSUR",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Framework :: Plone",
+        "Development Status :: 4 - Beta",
+        "Framework :: Plone :: 4.1",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
         "Programming Language :: Python",
+        "Topic :: Office/Business :: News/Diary",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='diazo theme plone teleSUR',
+      keywords='diazo theme plone telesur',
       author='',
       author_email='',
       url='https://github.com/desarrollotv/telesur.theme',
