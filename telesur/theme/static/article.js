95d9c666abc15c0f8eda94ef4f94ab40c3ce4e90
Gonzalo Almeida <flecox@gmail.com>
2012-03-28 11:54:38 -0300

    agregando manejo de solo audio de los videos

--- telesur/theme/static/article.js
+++ telesur/theme/static/article.js
@@ -27,15 +27,15 @@ jq(function($) {
         
         /*moving thumbs into his own div*/
         var thumbs = $('#video-galery-container .scrollable-video-galery .thumb-image').detach();
-        var items = $('.scrollable-video-galery').append('<div class="items"/>').find('.items');
+        var items = $('#video-galery-container .scrollable-video-galery').append('<div class="items"/>').find('.items');
         items.html(thumbs);
         
         /*moving videos outside the container into the video wrapper*/
-        var videos = $('.scrollable-video-galery .video-widget').detach();
-        $('.videos-wrapper').html(videos);
+        var videos = $('#video-galery-container .scrollable-video-galery .video-widget').detach();
+        $('#video-galery-container .videos-wrapper').html(videos);
         
         /*lets hide all the videos but the first*/
-        $('.videos-wrapper .video-widget').css('display', 'none').filter(":first").css('display', 'block');
+        $('#video-galery-container .videos-wrapper .video-widget').css('display', 'none').filter(":first").css('display', 'block');
         
         $(".video-counter .counter").prepOverlay({
             subtype:'inline',
@@ -47,11 +47,57 @@ jq(function($) {
         
         /*functionality for the gallery*/
         /*thumbs selectors*/
-        $('.items .thumb-image').click(function(){
+        $('#video-galery-container .items .thumb-image').click(function(){
             $('.videos-wrapper .video-widget').hide();
             $(this).data('video').show();
         });
     }
+    
+    
+    
+    
+    /**
+    * Audio scrollable gallery 
+    **/
+    /*first we have to check if we have videos*/
+    
+    if ($('#audio-galery-container .scrollable-video-galery img')[0] !== undefined) {
+                /*we have to pre preocess the html in order to get the correct gallery disposition*/
+                
+                /*lest do a relatinship thumb => video*/
+                $('#audio-galery-container .scrollable-video-galery .thumb-image').each(function(){
+                    $(this).data('audio', $(this).parent('.audio-widget'));
+                });
+                
+                /*moving thumbs into his own div*/
+                var thumbs = $('#audio-galery-container .scrollable-video-galery .thumb-image').detach();
+                var items = $('#audio-galery-container .scrollable-video-galery').append('<div class="items"/>').find('.items');
+                items.html(thumbs);
+                
+                /*moving videos outside the container into the video wrapper*/
+                var audios = $('#audio-galery-container .scrollable-video-galery .audio-widget').detach();
+                $('#audio-galery-container .videos-wrapper').html(audios);
+                
+                /*lets hide all the videos but the first*/
+                $('#audio-galery-container .videos-wrapper .audio-widget').css('display', 'none').filter(":first").css('display', 'block');
+                
+                $(".audio-counter .counter").prepOverlay({
+                    subtype:'inline',
+                    target: '#audio-galery-container'
+                });
+        
+                $("#audio-galery-container").scrollable({size: 4});
+                
+                
+                /*functionality for the gallery*/
+                /*thumbs selectors*/
+                $('#audio-galery-container .scrollable-video-galery .items .thumb-image').click(function(){
+                    $('.videos-wrapper .audio-widget').hide();
+                    $(this).data('audio').show();
+                });
+            }
+            
+    
     
     
     //article view, add videos from d&d widget
